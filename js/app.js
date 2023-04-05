07ce0e287c2ecd018902a828a84da61881f1882a
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-05 11:36:25 -0300

    se agrega un el tema elegido al localstorage para mantenerlo en el timepo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+//se ejecutan l principio de la pagina
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+cambiarTema(temaConfigurado);
+
 document.getElementById('btnDark').addEventListener('click', ()=> cambiarTema('dark'));
 document.getElementById('btnLight').addEventListener('click', ()=> cambiarTema('light'));
 
@@ -5,5 +9,9 @@ document.getElementById('btnLight').addEventListener('click', ()=> cambiarTema('
     let body = document.querySelector('body');
     body.setAttribute('data-bs-theme',color);
 
-    (color === 'dark')? document.getElementById('iconTheme').className = 'bi bi-moon-stars-fill' : document.getElementById('iconTheme').className = 'bi bi-brightness-high-fill'
+    (color === 'dark')? document.getElementById('iconTheme').className = 'bi bi-moon-stars-fill' 
+    : document.getElementById('iconTheme').className = 'bi bi-brightness-high-fill'
+
+    //guarda el tema en el local storage
+    localStorage.setItem('tema',JSON.stringify(color));
  }
