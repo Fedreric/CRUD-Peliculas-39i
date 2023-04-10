890fc31f0f9a829d1c84117baff8d84667fb32b9
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-10 10:45:31 -0300

    Podemos crear peliculas desde el archivo administrador.js

---
+++ js/administrador.js
@@ -0,0 +1,5 @@
+import Pelicula from "./classPelicula.js";
+//creae una nueva peli
+let nuevaPeli = new Pelicula('Super mari','descripcion','url','aventura',2023,'2hs','EEUU','personas');
+
+console.log(nuevaPeli);
