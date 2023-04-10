c401a021d6c42dc871ac4bcfadfbfd65ab2e6638
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-10 11:32:29 -0300

    Se realiza el acceso a la ventana modal con el boton crear pelicula

--- js/administrador.js
+++ js/administrador.js
@@ -1,6 +1,11 @@
 import Pelicula from "./classPelicula.js";
 const btnEditar = document.getElementById("btnEditar");
+const btnAgregar = document.getElementById("btnAgregar");
+const formularioPelicula = document.getElementById("formAdminPelicula");
+const modalPelicula = new bootstrap.Modal(document.getElementById("modalAgregar"));
 btnEditar.addEventListener("click", creaePeli);
+btnAgregar.addEventListener("click", mostrarModalPelicula);
+formularioPelicula.addEventListener("submit",cargarPelicula);
 //creae una nueva peli
 let nuevaPeli = new Pelicula('Super mari','descripcion','url','aventura',2023,'2hs','EEUU','-');
 
@@ -8,4 +13,14 @@ let nuevaPeli = new Pelicula('Super mari','descripcion','url','aventura',2023,'2
 
 function creaePeli(){
     console.log(nuevaPeli);
+}
+
+function mostrarModalPelicula(){
+    modalPelicula.show();
+}
+
+function cargarPelicula(e){
+    e.preventDefault();
+    console.log('creando pelicula...');
+    modalPelicula.hide();
 }
