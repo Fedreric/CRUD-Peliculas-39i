593fe3519df8045a6ce13e13760cb7abf0b620fa
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-23 16:37:56 -0300

    se agrega la logica del read, queda funcionando

--- js/readIndex.js
+++ js/readIndex.js
@@ -9,3 +9,34 @@ if (!listaPeliculas) {
 }
 
 console.log(listaPeliculas);
+
+cargaInicial();
+function cargaInicial() {
+    if (listaPeliculas.length > 0) {
+        //dibuja la tabla
+        listaPeliculas.map((pelicula, indice) => crearFila(pelicula, indice))
+    } else {
+        let contenederPeliculas = document.getElementById('contenederPeliculas');
+        contenederPeliculas.innerHTML += `
+        <p class="display-1 text-center fst-italic my-5">No se encontraron peliculas disponibles</p>
+    `
+    }
+}
+
+function crearFila(pelicula, indice) {
+    //aqui dibujo el TR
+    let contenederPeliculas = document.getElementById('contenederPeliculas');
+    contenederPeliculas.innerHTML += `
+    <article class=" col-md-6 col-lg-3 mb-3">
+    <div class="card h-100">
+        <img src="${pelicula.imagen}" class="card-img-top" alt="imagen de: ${pelicula.titulo}">
+        <div class="card-body">
+          <h5 class="card-title">${pelicula.titulo}</h5>
+        </div>
+        <div class="card-footer">
+            <a href="pages/detalles.html" class="btn btn-primary">Ver mas</a>
+        </div>
+      </div>
+    </article>
+    `
+}
